46f80bc255f48db483425d481dc36bac86444eed
pratik_03 <pratikagnihotri1234@gmail.com>
2023-02-20 12:07:40 +0530

    first commit

---
+++ static/js/index.js
@@ -0,0 +1,18 @@
+function bottom(){
+    window.scrollTo({
+        top:10000,
+        behavior:"smooth"
+    });
+}
+function about(){
+    window.scrollTo({
+        top:400,
+        behavior:"smooth"
+    });
+}
+function upper(){
+    window.scrollTo({
+        top:-1000000,
+        behavior:"smooth"
+    });
+}
